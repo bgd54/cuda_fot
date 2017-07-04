4291d7656afc44b5d36ea8b7f706ffefe772a1ee
bgd54 <balogh.dani54@gmail.com>
2017-07-04 09:16:17 +0200

    CPU OMP implementation modification

--- problem.hpp
+++ problem.hpp
@@ -48,7 +48,7 @@ template <unsigned Dim = 1, bool SOA = false> struct Problem {
   void loopGPUEdgeCentred(MY_SIZE num, MY_SIZE reset_every = 0);
   void loopGPUHierarchical(MY_SIZE num, MY_SIZE reset_every = 0);
 
-  void stepCPUEdgeCentred(float *temp) {
+  void stepCPUEdgeCentred(float *temp) {/*{{{*/
     for (MY_SIZE edge_ind = 0; edge_ind < graph.numEdges(); ++edge_ind) {
       MY_SIZE ind_left_base = graph.edge_to_node[graph.edge_to_node.getDim() * edge_ind];
       MY_SIZE ind_right_base = graph.edge_to_node[graph.edge_to_node.getDim() * edge_ind +
@@ -66,7 +66,7 @@ template <unsigned Dim = 1, bool SOA = false> struct Problem {
         point_weights[w_ind_left] += edge_weights[edge_ind] * temp[w_ind_right];
       }
     }
-  }
+  }/*}}}*/
 
   void loopCPUEdgeCentred(MY_SIZE num, MY_SIZE reset_every = 0) {/*{{{*/
     float *temp = (float *)malloc(sizeof(float) * graph.numPoints() *
@@ -93,7 +93,7 @@ template <unsigned Dim = 1, bool SOA = false> struct Problem {
   }/*}}}*/
 
   void stepCPUEdgeCentredOMP(const std::vector<MY_SIZE> &inds,
-                             data_t<float> &out) {
+                             data_t<float> &out) {/*{{{*/
     #pragma omp parallel for
     for (MY_SIZE i = 0; i < inds.size(); ++i) {
       MY_SIZE ind = inds[i];
@@ -109,41 +109,41 @@ template <unsigned Dim = 1, bool SOA = false> struct Problem {
           w_ind_left  = d + Dim * ind_left_base;
           w_ind_right = d + Dim * ind_right_base; 
         }
-        out[w_ind_right] += edge_weights[ind] * point_weights[w_ind_left];
-        out[w_ind_left] += edge_weights[ind] * point_weights[w_ind_right];
+        point_weights[w_ind_right] += edge_weights[ind] * out[w_ind_left];
+        point_weights[w_ind_left] += edge_weights[ind] * out[w_ind_right];
       }
     }
-  }
+  }/*}}}*/
 
-  void loopCPUEdgeCentredOMP(MY_SIZE num, MY_SIZE reset_every = 0) {
+  void loopCPUEdgeCentredOMP(MY_SIZE num, MY_SIZE reset_every = 0) {/*{{{*/
     data_t<float> temp(point_weights.getSize(), point_weights.getDim());
     std::vector<std::vector<MY_SIZE>> partition = graph.colourEdges();
     MY_SIZE num_of_colours = partition.size();
-    #pragma omp parallel for
-    for (MY_SIZE e = 0; e < point_weights.getSize() * point_weights.getDim();
-         ++e) {
-      temp[e] = point_weights[e];
+    //TODO
+    int num_edges = 0;
+    for(std::vector<MY_SIZE> &color : partition){
+      num_edges += color.size();
     }
+    std::cout<< num_edges << " " << graph.numEdges()<< std::endl;
+
+
     TIMER_START(t);
     for (MY_SIZE i = 0; i < num; ++i) {
-      for (MY_SIZE c = 0; c < num_of_colours; ++c) {
-        stepCPUEdgeCentredOMP(partition[c], temp);
-      }
       TIMER_TOGGLE(t);
       #pragma omp parallel for
-      for (MY_SIZE e = 0; e < point_weights.getSize() * point_weights.getDim();
-           ++e) {
-        point_weights[e] = temp[e];
-      }
-      if (reset_every && i % reset_every == reset_every - 1) {
-        reset();
-        #pragma omp parallel for
-        for (MY_SIZE e = 0;
-             e < point_weights.getSize() * point_weights.getDim(); ++e) {
-          temp[e] = point_weights[e];
-        }
+      for (MY_SIZE e = 0;
+           e < point_weights.getSize() * point_weights.getDim(); ++e) {
+        temp[e] = point_weights[e];
       }
       TIMER_TOGGLE(t);
+      for (int c = num_of_colours-1; c >=0; --c) {
+        stepCPUEdgeCentredOMP(partition[c], temp);
+      }
+      if (reset_every && i % reset_every == reset_every - 1) {
+        TIMER_TOGGLE(t);
+        reset();
+        TIMER_TOGGLE(t);
+      }
     }
     PRINT_BANDWIDTH(
         t, "loopCPUEdgeCentredOMP",
@@ -151,7 +151,7 @@ template <unsigned Dim = 1, bool SOA = false> struct Problem {
             num,
         sizeof(float) * (2.0 * Dim * graph.numPoints() + graph.numEdges()) *
             num);
-  }
+  }/*}}}*/
 
   void reorder() { graph.reorder(edge_weights, &point_weights); }
 };
