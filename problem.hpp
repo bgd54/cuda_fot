48339f6bdd67e8fae8fbdeefccf088f9c7fd95ad
sulan <sulyok.a.attila@gmail.com>
2017-09-18 16:42:12 +0100

    Fix Problem (partition) IO

--- problem.hpp
+++ problem.hpp
@@ -47,24 +47,35 @@ struct Problem {
   }
 
   Problem(std::istream &graph_is, MY_SIZE _block_size = DEFAULT_BLOCK_SIZE,
-          std::istream *coord_is = nullptr, std::istream *partition_is = nullptr)
+          std::istream *coord_is = nullptr,
+          std::istream *partition_is = nullptr)
       : graph(graph_is, coord_is), edge_weights(graph.numEdges()),
         point_weights(graph.numPoints()), block_size{_block_size} {
     if (partition_is != nullptr) {
       if (!(*partition_is)) {
-        throw InvalidInputFile{"partition input",0};
+        throw InvalidInputFile{"partition input", 0};
       }
       partition_vector.resize(graph.numEdges());
-    }
-    for (MY_SIZE i = 0; i < graph.numEdges(); ++i) {
-      edge_weights[i] = DataType(rand() % 10000 + 1) / 5000.0;
-      edge_weights[i] *= 0.001;
-      if (partition_is != nullptr) {
+      int read_block_size;
+      *partition_is >> read_block_size;
+      if (!(*partition_is)) {
+        throw InvalidInputFile{"partition input", 1};
+      }
+      if (read_block_size != block_size) {
+        std::cerr << "Warning: block size in file (" << read_block_size
+                  << ") doesn't equal to used block size (" << block_size << ")"
+                  << std::endl;
+      }
+      for (MY_SIZE i = 0; i < graph.numEdges(); ++i) {
         *partition_is >> partition_vector[i];
         if (!(*partition_is)) {
-          throw InvalidInputFile{"partition input",i};
+          throw InvalidInputFile{"partition input", i + 1};
         }
       }
+    }
+    for (DataType &weight : edge_weights) {
+      weight = DataType(rand() % 10000 + 1) / 5000.0;
+      weight *= 0.001;
     }
     reset();
   }
@@ -222,6 +233,7 @@ struct Problem {
   }
 
   void writePartition(std::ostream &os) const {
+    os << block_size << std::endl;
     for (MY_SIZE p : partition_vector) {
       os << p << std::endl;
     }
@@ -229,13 +241,23 @@ struct Problem {
 
   void readPartition(std::istream &partition_is) {
     if (!partition_is) {
-      throw InvalidInputFile{"partition input",0};
+      throw InvalidInputFile{"partition input", 0};
+    }
+    int read_block_size;
+    partition_is >> read_block_size;
+    if (!partition_is) {
+      throw InvalidInputFile{"partition input", 1};
+    }
+    if (read_block_size != block_size) {
+      std::cerr << "Warning: block size in file (" << read_block_size
+                << ") doesn't equal to used block size (" << block_size << ")"
+                << std::endl;
     }
     partition_vector.resize(graph.numEdges());
     for (MY_SIZE i = 0; i < graph.numEdges(); ++i) {
       partition_is >> partition_vector[i];
       if (!partition_is) {
-        throw InvalidInputFile{"partition input",i};
+        throw InvalidInputFile{"partition input", i + 1};
       }
     }
   }
