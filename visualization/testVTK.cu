ecaac27052f5bac4923ab71faf686d594ffda098
sulan <sulyok.a.attila@gmail.com>
2018-01-09 13:51:04 +0000

    Add reuse/threadcol statistics to visualisation

--- visualization/testVTK.cu
+++ visualization/testVTK.cu
@@ -32,20 +32,26 @@ void writeGlobalColouringVTK(const std::string &filename,
 void writeHierarchicalColouringVTK(const std::string &filename,
                                    const Problem<false> &problem,
                                    const data_t &point_coordinates) {
-  const HierarchicalColourMemory<false> memory(
-      problem, problem.partition_vector);
+  const HierarchicalColourMemory<false> memory(problem,
+                                               problem.partition_vector);
   const MY_SIZE MESH_DIM = problem.mesh.cell_to_node[0].getDim();
   std::cout << "Number of block colours: " << memory.colours.size()
             << std::endl;
-  data_t cell_list(data_t::create<MY_SIZE>(problem.mesh.numCells(),
-                                           MESH_DIM));
+  data_t cell_list(data_t::create<MY_SIZE>(problem.mesh.numCells(), MESH_DIM));
   std::vector<std::vector<std::uint16_t>> data(3);
   MY_SIZE blk_col_ind = 0, blk_ind = 0;
   MY_SIZE cell_ind = 0;
+  double avg_thread_col = 0;
+  std::size_t total_cache_size = 0;
+  std::size_t total_num_blocks = 0;
   for (const auto &m : memory.colours) {
+    double num_cell_colours = std::accumulate(m.num_cell_colours.begin(),
+                                              m.num_cell_colours.end(), 0.0);
+    avg_thread_col += num_cell_colours;
+    total_cache_size += m.points_to_be_cached.size();
+    total_num_blocks += m.num_cell_colours.size();
     data[VTK_IND_THR_COL].insert(data[VTK_IND_THR_COL].end(),
-                                 m.cell_colours.begin(),
-                                 m.cell_colours.end());
+                                 m.cell_colours.begin(), m.cell_colours.end());
     data[VTK_IND_BLK_COL].insert(data[VTK_IND_BLK_COL].end(),
                                  m.cell_colours.size(), blk_col_ind++);
     assert(MESH_DIM * m.cell_colours.size() == m.cell_list[0].size());
@@ -75,6 +81,13 @@ void writeHierarchicalColouringVTK(const std::string &filename,
   }
   assert(cell_ind == cell_list.getSize());
   writeMeshToVTKAscii(filename, cell_list, point_coordinates, data);
+  std::cout << "Average number of thread colours: "
+            << avg_thread_col / total_num_blocks << "\n"
+            << "Average reuse factor: "
+            << static_cast<double>(total_cache_size) /
+                   (problem.mesh.numCells() *
+                    problem.mesh.cell_to_node[0].getDim())
+            << "\n";
 }
 
 void writePartitionVTK(const std::string &filename,
